47cf3b8de20d79e2c6c6a18ce22cedfb63d1660a
Art943 <artush.simonjan@yaelev.se>
2024-06-11 12:25:32 +0200

    trying

--- desktop/client/testfold/main.cpp
+++ desktop/client/testfold/main.cpp
@@ -3,6 +3,7 @@
 #include <QGridLayout>
 #include <QFont>
 #include <QLabel>
+#include <QPainter>
 class Window : public QDialog
 {
 public:
@@ -15,23 +16,31 @@ public:
 
         // Create a QFont object using the Material Icons font
         QFont materialIconsFont(materialIconsFontFamily);
-        materialIconsFont.setPointSize(48);
+        materialIconsFont.setPointSize(110);
 
         // Set a fixed size for the window
-        const int fixedWidth = 600;
-        const int fixedHeight = 400;
+        const int fixedWidth = 800;
+        const int fixedHeight = 560;
         setMinimumSize(fixedWidth, fixedHeight);
         setMaximumSize(fixedWidth, fixedHeight);
 
         // Set the background color
         setStyleSheet("background-color: rgb(64, 32, 60);");
         // QFont sansFont("0xebdc", 12);
-        QLabel *label = new QLabel(QChar(0xe9e4)); // Specific icon character
+        QLabel *label = new QLabel(QChar(0xebdc)); // Specific icon character
         label->setFont(materialIconsFont);
-        label->setAlignment(Qt::AlignCenter);
 
         // Add the label to the layout
-        layout->addWidget(label, 0, 0);
+        layout->addWidget(label, 1, 1);
+        layout->setAlignment(Qt::AlignCenter | Qt::AlignRight);
+
+        QRectF rectangle(10.0, 20.0, 80.0, 60.0);
+        int startAngle = 30 * 16;
+        int spanAngle = 120 * 16;
+
+        QPainter painter(this);
+        painter.drawArc(rectangle, startAngle, spanAngle);
+        layout->addWidget(painter.drawArc(rectangle, startAngle, spanAngle), Qt::AlignCenter);
     }
 };
 
