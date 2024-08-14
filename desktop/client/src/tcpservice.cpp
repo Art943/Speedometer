346531e4c523263cab44358edd3bf8901400064d
GyroRaven <joel_husberg@live.se>
2024-08-14 12:49:57 +0200

    progress

--- desktop/client/src/tcpservice.cpp
+++ desktop/client/src/tcpservice.cpp
@@ -1,1 +1,55 @@
 #include "tcpservice.h"
+
+void TCPService::run(void)
+{
+    // Create socket and check
+    socketID = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (socketID == -1)
+    {
+        std::cout << "Failed to create the socket..." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in servaddr;
+
+    // Assign IP and PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(Setting::TCP::PORT);
+    servaddr.sin_addr.s_addr = inet_addr(Setting::TCP::IP);
+
+    while (running)
+    {
+        // Connect to the server
+        if (0 != connect(socketID, (sockaddr *)&servaddr, sizeof(servaddr)))
+        {
+            // Connection Failed
+            close(socketID);
+            std::cout << "Connection to the server failed..." << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+        else
+        {
+            // Connection Succeeded
+            connectionStatus = true;
+        }
+
+        while (isConnected())
+        {
+            uint8_t temp_buffer[Setting::Signal::BUFFER_LENGTH];
+
+            // Receive data from the server and store it in buffer
+            if (Setting::Signal::BUFFER_LENGTH != read(socketID, temp_buffer, Setting::Signal::BUFFER_LENGTH))
+            {
+                // std::cout << "Failed to recieve buffer..." << std::endl;
+                connectionStatus = false;
+                break;
+            }
+
+            mtx.lock();
+            memcpy(buffer, temp_buffer, Setting::Signal::BUFFER_LENGTH);
+            mtx.unlock();
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        }
+    }
+}
