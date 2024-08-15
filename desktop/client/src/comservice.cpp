d305b34c00ddbfd18baab538209f97c8ec1bf2f2
GyroRaven <joel_husberg@live.se>
2024-08-15 08:14:56 +0200

    ready for review

--- desktop/client/src/comservice.cpp
+++ desktop/client/src/comservice.cpp
@@ -10,25 +10,25 @@ void COMService::extract(int start, int length, uint32_t &value)
     int bitOffset{start % CHAR_BIT};
 
     // Variable to keep track of how many bits we've extracted
-    int extractedBitCount{0};
+    int extractedBits{0};
 
     mtx.lock();
-    while (extractedBitCount < length)
+    while (extractedBits < length)
     {
         // Target only the relevant bits in byte one
         uint8_t bitsLeftInByte = CHAR_BIT - bitOffset;
 
         // The amount of bits to extract; Either how many bits are left in the current byte, or the rest of the bits we want extracted, whichever is less.
-        uint8_t bitsToExtract = (bitsLeftInByte < (length - extractedBitCount)) ? bitsLeftInByte : (length - extractedBitCount);
+        uint8_t bitsToExtract = (bitsLeftInByte < (length - extractedBits)) ? bitsLeftInByte : (length - extractedBits);
 
         // Make a mask covering the bits we want to extract from the current byte
         uint8_t mask = ((1 << bitsToExtract) - 1) << bitOffset;
 
         // Extract the bits we want from the current byte in the buffer
-        uint8_t extractedBits = (buffer[bufferIndex] & mask) >> bitOffset;
+        uint8_t extractedData = (buffer[bufferIndex] & mask) >> bitOffset;
 
         // Store extracted bits in it's proper place in "value"
-        value |= static_cast<uint32_t>(extractedBits) << extractedBitCount;
+        value |= static_cast<uint32_t>(extractedData) << extractedBits;
 
         extractedBits += bitsToExtract;
         bitOffset = 0;
