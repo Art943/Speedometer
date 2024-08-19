bd15fbe3b2b42d239389fcf46d92852c8a7059af
Alexander Markantonis <alexander.markantonis@gmail.com>
2024-08-19 21:25:37 +0200

    Swapped the ports and hardcoded baudrate buffer size for troubleshooting

--- esp32/client/src/main.cpp
+++ esp32/client/src/main.cpp
@@ -1,7 +1,7 @@
 #include <CAN.h>
 #include <Arduino.h>
 #include <CAN_config.h>
-#include "setting.h"
+// #include "setting.h"
 
 constexpr int MSG_ID = 0x100;
 
@@ -9,13 +9,10 @@ CAN_device_t CAN_cfg;
 
 void setup()
 {
-    Serial.begin(SETTING::CAN::Baudrate);
-    // Serial.begin(SETTING::CAN::Baudrate, SERIAL_8N1);
-    while (!Serial)
-    {
-        delay(10);
-    }
-    Serial.println("Ready to send data...");
+    delay(2000);
+    Serial.begin(115200);
+    // Serial.begin(SETTING::CAN::Baudrate);
+    Serial.println("Ready to receive data...");
 
     // Config the communication
     CAN_cfg.tx_pin_id = GPIO_NUM_5;
@@ -37,12 +34,6 @@ void loop()
             Serial.write(frame.data.u8, frame.FIR.B.DLC);
         }
     }
-    // Buffer to hold data
-    // uint8_t buffer[3];
 
-    // Send the data
-    // Serial.write(buffer, sizeof(buffer));
-
-    // Wait before sending the next set of data
-    delay(20);
+    delay(10);
 }
--- esp32/server/src/main.cpp
+++ esp32/server/src/main.cpp
@@ -1,7 +1,7 @@
 #include <CAN.h>
 #include <Arduino.h>
 #include <CAN_config.h>
-#include "setting.h"
+// #include "setting.h"
 
 constexpr int MSG_ID = 0x100;
 
@@ -9,13 +9,10 @@ CAN_device_t CAN_cfg;
 
 void setup()
 {
-    Serial.begin(SETTING::CAN::Baudrate);
-    // Serial.begin(SETTING::CAN::Baudrate, SERIAL_8N1);
-    while (!Serial)
-    {
-        delay(10);
-    }
-    Serial.println("Ready to recieve data...");
+    delay(2000);
+    Serial.begin(115200);
+    // Serial.begin(SETTING::CAN::Baudrate);
+    Serial.println("Ready to send data...");
 
     // Config the communication
     CAN_cfg.tx_pin_id = GPIO_NUM_5;
@@ -28,40 +25,21 @@ void setup()
 
 void loop()
 {
+    while (!Serial.available())
+    {
+        ;
+    }
+
     CAN_frame_t frame{0};
 
-    frame.MsgID = MSG_ID;                             // The identifier of the receiver
-    frame.FIR.B.RTR = CAN_no_RTR;                     // Remote transmition set to 0, means this is a data frame
-    frame.FIR.B.FF = CAN_frame_std;                   // Indicates standard ll bits long frame
-    frame.FIR.B.DLC = SETTING::Signal::BUFFER_LENGTH; // Data Length Code, the length of the data
+    frame.MsgID = MSG_ID;           // The identifier of the receiver
+    frame.FIR.B.RTR = CAN_no_RTR;   // Remote transmition set to 0, means this is a data frame
+    frame.FIR.B.FF = CAN_frame_std; // Indicates standard ll bits long frame
+    frame.FIR.B.DLC = 3;            // SETTING::Signal::BUFFER_LENGTH; // Data Length Code, the length of the data
 
-    if (SETTING::Signal::BUFFER_LENGTH == Serial.readBytes(frame.data.u8, SETTING::Signal::BUFFER_LENGTH))
+    // if (SETTING::Signal::BUFFER_LENGTH == Serial.readBytes(frame.data.u8, SETTING::Signal::BUFFER_LENGTH))
+    if (3 == Serial.readBytes(frame.data.u8, 3))
     {
         CAN_write_frame(&frame);
-        // static uint8_t buffer[3];
-        // static size_t bytesReceived = 0;
-
-        // while (Serial.available() > 0)
-        // {
-        //     buffer[bytesReceived] = Serial.read(); // Read a byte
-        //     bytesReceived++;
-
-        //     if (bytesReceived == sizeof(buffer))
-        //     {
-        // Once the buffer is full, process the data
-
-        // // !!! Placeholder used for testing, replace with proper handling of data !!!
-        // Serial.print("Received data: ");
-        // for (size_t i = 0; i < sizeof(buffer); i++)
-        // {
-        //     Serial.print(buffer[i], HEX);
-        //     Serial.print(" ");
-        // }
-        // Serial.println();
-        // // !!! End of placeholder !!!
-
-        // Reset the counter for the next set of data
-        // bytesReceived = 0;
-        // }
     }
 }
