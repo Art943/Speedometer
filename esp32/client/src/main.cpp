0526576b9c40ff76a4eb12255c77a38ac4425935
Art943 <145445020+Art943@users.noreply.github.com>
2024-08-21 13:53:19 +0200

    Update main.cpp

--- esp32/client/src/main.cpp
+++ esp32/client/src/main.cpp
@@ -17,13 +17,6 @@ void loop()
     // Buffer to hold data
     uint8_t buffer[3];
 
-    // !!! Test code to be removed and replaced with proper data handling !!!
-    for (size_t i = 0; i < sizeof(buffer); i++)
-    {
-        buffer[i] = random(0, 256);
-    }
-    // !!! End of test code !!!
-
     // Send the data
     Serial.write(buffer, sizeof(buffer));
 
